a41834db3a4d3f4a9470115a307fdc03f74e785d
Stuart Herbert <stuart@stuartherbert.com>
2023-11-08 11:15:42 +0000

    Fix: improve presentation of pre blocks

--- src/typography/fonts.js
+++ src/typography/fonts.js
@@ -3,6 +3,6 @@ module.exports = {
         '--imprint-sans-serif': 'Lato, Helvetica, Roboto, Arial, sans-serif !default',
         '--imprint-serif': 'serif',
         '--imprint-system': 'system-ui',
-        '--imprint-monospace': "Menlo, Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+        '--imprint-monospace': "Monaco, Menlo, Consolas, 'Andale Mono', 'Ubuntu Mono', monospace",
     }
 }
--- src/typography/pre.js
+++ src/typography/pre.js
@@ -3,22 +3,28 @@ import { rgb } from "../helpers/colors";
 
 module.exports = {
     preVars: {
+        '--imprint-block-mono-border-radius': '0.25rem',
+
         '--imprint-inline-mono-size': '0.85em',
         '--imprint-inline-mono-border-radius': '0.25rem',
+        '--imprint-inline-mono-padding': '0.1rem 0.2rem',
 
-        '--imprint-code-bg': rgb(colorDetails['imprint-palegray'].rgbChannels, '0.75'),
+        '--imprint-code-bg': rgb(colorDetails['imprint-palegray'].rgbChannels, '0.66'),
         '--imprint-code-fg': colorDetails['ganbaro-red-500'].hex,
         '--imprint-code-border-fg': colorDetails['imprint-lightgray'].hex,
         '--imprint-code-border': '1px solid var(--imprint-code-border-fg)',
-        '--imprint-code-border-bottom': '2px solid var(--imprint-code-border-fg)',
-        '--imprint-code-padding': '0.1rem 0.2rem 1px 0.2rem',
+        '--imprint-code-padding': 'var(--imprint-inline-mono-padding)',
         '--imprint-code-border-radius': 'var(--imprint-inline-mono-border-radius)',
 
         '--imprint-pre-fg': colorDetails['imprint-nero'].hex,
-        '--imprint-pre-bg': rgb(colorDetails['imprint-palegray'].rgbChannels, '0.75'),
+        '--imprint-pre-bg': rgb(colorDetails['imprint-palegray'].rgbChannels, '0.66'),
+        '--imprint-pre-padding': '0.5rem 1rem',
+        '--imprint-pre-border-radius': 'var(--imprint-block-mono-border-radius)',
 
-        '--imprint-samp-bg': rgb(colorDetails['imprint-palegray'].rgbChannels, '0.75'),
+        '--imprint-samp-fg': 'inherit',
+        '--imprint-samp-bg': rgb(colorDetails['imprint-palegray'].rgbChannels, '0.66'),
         '--imprint-samp-border-radius': 'var(--imprint-inline-mono-border-radius)',
+        '--imprint-samp-padding': 'var(--imprint-inline-mono-padding)',
     },
     preStyles: {
         'code, kbd, pre, samp': {
@@ -31,23 +37,36 @@ module.exports = {
             'background-color': 'var(--imprint-code-bg)',
             'color': 'var(--imprint-code-fg)',
             'border': 'var(--imprint-code-border)',
-            'border-bottom': 'var(--imprint-code-border-bottom)',
             'border-radius': 'var(--imprint-code-border-radius)',
         },
 
         'samp': {
             'font-size': 'var(--imprint-inline-mono-size)',
-            'padding': 'var(--imprint-inline-text-padding)',
+            'padding': 'var(--imprint-samp-padding)',
             'background-color': 'var(--imprint-samp-bg)',
+            'color': 'var(--imprint-samp-fg)',
             'border': 'var(--imprint-code-border)',
             'border-radius': 'var(--imprint-samp-border-radius)',
         },
 
+        "pre": {
+            '@apply bodycopy-block bodycopy-fonts bodycopy-spacing': {},
+            'font-size': 'var(--imprint-inline-mono-size)',
+            'color': 'var(--imprint-pre-fg)',
+            'background-color': 'var(--imprint-pre-bg)',
+            'border': 'var(--imprint-code-border)',
+            'padding': 'var(--imprint-pre-padding)',
+            'border-radius': 'var(--imprint-pre-border-radius)',
+        },
+
         "pre code": {
+            'font-size': 'inherit',
             "padding": 0,
+            "border": 0,
             "color": "var(--imprint-pre-fg)",
             "background-color": "var(--imprint-pre-bg)",
             "white-space": "pre-wrap",
+            "border-radius": 0,
         },
 
         "a code": {
