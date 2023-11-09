35e3bbce3cf5778c0b0e4c23fa97e11ebe648390
Stuart Herbert <stuart@stuartherbert.com>
2023-11-09 17:19:41 +0000

    Docs: frontmatter now works in MDX files

--- docs/imprintcss.com/mdx/remark.mjs
+++ docs/imprintcss.com/mdx/remark.mjs
@@ -1,7 +1,11 @@
 import { mdxAnnotations } from 'mdx-annotations'
 import remarkGfm from 'remark-gfm'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 
 export const remarkPlugins = [
   mdxAnnotations.remark,
   remarkGfm,
+  remarkFrontmatter,
+  remarkMdxFrontmatter,
 ]
