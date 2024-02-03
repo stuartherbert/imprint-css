70a64bc61173b37b99b847990c3591bf49bad907
Stuart Herbert <stuart@stuartherbert.com>
2024-02-03 12:51:25 +0000

    Fix: change sans-serif font to default to system-ui instead

--- src/typography/fonts.js
+++ src/typography/fonts.js
@@ -1,6 +1,6 @@
 module.exports = {
     'fontVars': {
-        '--imprint-sans-serif': 'Lato, Helvetica, Roboto, Arial, sans-serif !default',
+        '--imprint-sans-serif': 'system-ui, sans-serif !default',
         '--imprint-serif': 'serif',
         '--imprint-system': 'system-ui',
         '--imprint-monospace': "Monaco, Menlo, Consolas, 'Andale Mono', 'Ubuntu Mono', monospace",
