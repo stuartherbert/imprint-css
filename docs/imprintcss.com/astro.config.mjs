54140373c3e50b314aa41e86c4c00861b8588fec
Stuart Herbert <stuart@stuartherbert.com>
2024-02-04 11:22:30 +0000

    WIP: use Astro for the docs site

---
+++ docs/imprintcss.com/astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    syntaxHighlight: false,
+  },
+  integrations: [
+    mdx(),
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      applyBaseStyles: false,
+    }),
+  ],
+});
