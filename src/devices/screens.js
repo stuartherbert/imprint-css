b9d4b1f7308345628f657d383c1218ca386c6d10
Stuart Herbert <stuart@stuartherbert.com>
2024-06-29 16:28:30 +0100

    Fix: redesign device groups to be easier to work with

--- src/devices/screens.js
+++ src/devices/screens.js
@@ -1,10 +1,12 @@
 module.exports = {
     theme: {
         screens: {
-            'a': { 'max': "319px"},
-            'b': { 'max': "599px" },
-            'c': { 'raw': '(min-width:600px) and (pointer:corse)' },
-            'd': { 'raw': '(min-width:600px) and (pointer:fine)' },
+            'a': { 'max': "319px" },
+            'b': { 'min': "320px" },
+            'c': { 'min': '600px' },
+            'c-t': { 'raw': '(min-width:600px) and (pointer:corse)' },
+            'c-d': { 'raw': '(min-width:600px) and (pointer:fine)' },
+            'd': { 'min': '1920px' },
          },
     },
     staticUtilities: {
@@ -22,7 +24,7 @@ module.exports = {
                 'max-width': '600px',
             },
             '.__imprint-container-d': {
-                'max-width': '600px',
+                'max-width': '800px',
             },
             '.imprint-container': {
                 '@apply a:__imprint-container-a b:__imprint-container-b c:__imprint-container-c d:__imprint-container-d': {},
