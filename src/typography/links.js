bc1b18b7f04c6cae5ba3e950338972cf5191080f
Stuart Herbert <stuart@stuartherbert.com>
2024-06-29 21:16:48 +0100

    Fix: links are styled once again

--- src/typography/links.js
+++ src/typography/links.js
@@ -57,16 +57,16 @@ module.exports = {
             '--imprint-link-active-border': colors['ganbaro-red']['500'],
         },
         styles: {
-            "not(nav) a, .imprint-link": {
+            ":not(nav) a, .imprint-link": {
                 "@apply __imprint-link": {},
             },
-            "not(nav) a:visited, imprint-link:visited": {
+            ":not(nav) a:visited, imprint-link:visited": {
                 "@apply __imprint-link-visited": {},
             },
-            "not(nav) a:hover, imprint-link:hover": {
+            ":not(nav) a:hover, imprint-link:hover": {
                 "@apply __imprint-link-hover": {},
             },
-            "not(nav) a:active, not(nav) a:focus, .imprint-link:active, .imprint-link:focus": {
+            ":not(nav) a:active, not(nav) a:focus, .imprint-link:active, .imprint-link:focus": {
                 "@apply __imprint-link-active": {},
             },
         },
