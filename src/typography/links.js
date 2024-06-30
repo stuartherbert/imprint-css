2785ce2f3221ec22b586089978cc0c4393d61b39
Stuart Herbert <stuart@stuartherbert.com>
2024-06-30 19:03:42 +0100

    Fix: imprint-link utility works again

--- src/typography/links.js
+++ src/typography/links.js
@@ -1,6 +1,37 @@
 const definitionStore = require("../helpers/definitionStore");
 const { colors } = require("../theme/colors");
 
+definitionStore.staticUtilities.styles = {
+    ...definitionStore.staticUtilities.styles,
+    ...{
+        ".imprint-link": {
+            'color': 'var(--imprint-link-color)',
+            'background-color': 'var(--imprint-link-background-color)',
+            'text-decoration': 'none',
+            'font-weight': 'inherit',
+            'border-bottom': '1px solid var(--imprint-link-border)',
+        },
+        ".imprint-link:visited, .imprint-link-visited": {
+            'color': 'var(--imprint-link-visited-color)',
+            'background-color': 'var(--imprint-link-visited-background-color)',
+            'text-decoration': 'none',
+            'font-weight': 'inherit',
+            'border-bottom': '1px solid var(--imprint-link-border)',
+        },
+        ".imprint-link:hover, .imprint-link-hover": {
+            'color': 'var(--imprint-link-hover-color)',
+            'background-color': 'var(--imprint-link-hover-background-color)',
+            'border-bottom': '1px solid var(--imprint-link-hover-border)',
+            'font-weight': 'inherit',
+        },
+        ".imprint-link:active, .imprint-link:focus, .imprint-link-active": {
+            'color': 'var(--imprint-link-active-color)',
+            'background-color': 'var(--imprint-link-active-background-color)',
+            'border-bottom': '1px solid var(--imprint-link-active-border)',
+            'font-weight': 'inherit',
+        },
+    }
+}
 definitionStore.defaultStyling.vars = {
     ...definitionStore.defaultStyling.vars,
     ...{
@@ -25,31 +56,17 @@ definitionStore.defaultStyling.vars = {
 definitionStore.defaultStyling.styles = {
     ...definitionStore.defaultStyling.styles,
     ...{
-        "a:not(nav a), .imprint-link": {
-            'color': 'var(--imprint-link-color)',
-            'background-color': 'var(--imprint-link-background-color)',
-            'text-decoration': 'none',
-            'font-weight': 'inherit',
-            'border-bottom': '1px solid var(--imprint-link-border)',
+        "a:not(nav a)": {
+            "@apply imprint-link": {},
         },
-        "a:visited:not(nav a), .imprint-link:visited": {
-            'color': 'var(--imprint-link-visited-color)',
-            'background-color': 'var(--imprint-link-visited-background-color)',
-            'text-decoration': 'none',
-            'font-weight': 'inherit',
-            'border-bottom': '1px solid var(--imprint-link-border)',
+        "a:visited:not(nav a)": {
+            "@apply imprint-link-visited": {},
         },
-        " a:hover:not(nav a), .imprint-link:hover": {
-            'color': 'var(--imprint-link-hover-color)',
-            'background-color': 'var(--imprint-link-hover-background-color)',
-            'border-bottom': '1px solid var(--imprint-link-hover-border)',
-            'font-weight': 'inherit',
+        " a:hover:not(nav a)": {
+            "@apply imprint-link-hover": {},
         },
-        "a:active:not(nav a), a:focus:not(nav), .imprint-link:active, .imprint-link:focus": {
-            'color': 'var(--imprint-link-active-color)',
-            'background-color': 'var(--imprint-link-active-background-color)',
-            'border-bottom': '1px solid var(--imprint-link-active-border)',
-            'font-weight': 'inherit',
+        "a:active:not(nav a), a:focus:not(nav)": {
+            "@apply imprint-link-active": {},
         },
     },
 }
