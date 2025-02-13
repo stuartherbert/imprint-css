046e71638cf53e32ef9d83f57e683ed8ff4eacba
Stuart Herbert <stuart@stuartherbert.com>
2025-02-13 09:16:35 +0000

    IMPT-0013 New: added iscc-nbs-applegreen color

--- packages/color-collections/src/colors/misc.ts
+++ packages/color-collections/src/colors/misc.ts
@@ -144,6 +144,7 @@ export const MISC_COLORS: CssColorDefinitions = {
     "internationalorange-aerospace": makeCssHexColorDefinition("#ff4f00"),
     "internationalorange-engineering": makeCssHexColorDefinition("#ba160c"),
     "internationalorange-ggb": makeCssHexColorDefinition("#f04a00"),
+    "iscc-nbs-applegreen": makeCssHexColorDefinition("#8ab800"),
     "iscc-nbs-applered": makeCssHexColorDefinition("#be0032"),
     "iscc-nbs-artichoke": makeCssHexColorDefinition("#8f9779"),
     "iscc-nbs-ashgrey": makeCssHexColorDefinition("#b2beb5"),
