08ed39164e7f0896ede836fc5918fb92947e754f
Stuart Herbert <stuart@stuartherbert.com>
2025-02-13 09:18:48 +0000

    IMPT-0013 Fix: renamed misc-fern to misc-ferngreen

--- packages/color-collections/src/colors/misc.ts
+++ packages/color-collections/src/colors/misc.ts
@@ -263,7 +263,7 @@ export const MISC_COLORS: CssColorDefinitions = {
     "misc-evergreen": makeCssHexColorDefinition("#05472a"),
     "misc-falured": makeCssHexColorDefinition("#801818"),
     "misc-feldgrau": makeCssHexColorDefinition("#4d5d53"),
-    "misc-fern": makeCssHexColorDefinition("#4f7942"),
+    "misc-ferngreen": makeCssHexColorDefinition("#4f7942"),
     "misc-fireenginered": makeCssHexColorDefinition("#ce2029"),
     "misc-flax": makeCssHexColorDefinition("#eedc82"),
     "misc-fluorescentred": makeCssHexColorDefinition("#ff2226"),
