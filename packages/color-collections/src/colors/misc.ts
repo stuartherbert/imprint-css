c55f25d63b4c6323176080754e923a51c9483a8d
Stuart Herbert <stuart@stuartherbert.com>
2025-02-13 16:26:19 +0000

    IMPT-0013 New: added iscc-nbs-operamauve color

--- packages/color-collections/src/colors/misc.ts
+++ packages/color-collections/src/colors/misc.ts
@@ -192,6 +192,7 @@ export const MISC_COLORS: CssColorDefinitions = {
     "iscc-nbs-mossgreen": makeCssHexColorDefinition("#8a9a5b"),
     "iscc-nbs-myrtlegreen": makeCssHexColorDefinition("#317873"),
     "iscc-nbs-oldrose": makeCssHexColorDefinition("#c08081"),
+    "iscc-nbs-operamauve": makeCssHexColorDefinition("#b784a7"),
     "iscc-nbs-palepink": makeCssHexColorDefinition("#f9ccca"),
     "iscc-nbs-pansypurple": makeCssHexColorDefinition("#78184a"),
     "iscc-nbs-pastelpink": makeCssHexColorDefinition("#dea5a4"),
