c16da2463cefffd8926a74aedc689df08463fe6b
Stuart Herbert <stuart@stuartherbert.com>
2025-02-14 09:37:31 +0000

    IMPT-0013 New: added crayola-sonicsilver color

--- packages/color-collections/src/colors/misc.ts
+++ packages/color-collections/src/colors/misc.ts
@@ -129,6 +129,7 @@ export const MISC_COLORS: CssColorDefinitions = {
     "crayola-shockingpink": makeCssHexColorDefinition("#ff6fff"),
     "crayola-skyblue": makeCssHexColorDefinition("#80daeb"),
     "crayola-smokeytopaz": makeCssHexColorDefinition("#832a0d"),
+    "crayola-sonicsilver": makeCssHexColorDefinition("#757575"),
     "crayola-silver": makeCssHexColorDefinition("#c9c0bb"),
     "crayola-steelpink": makeCssHexColorDefinition("#c3c"),
     "crayola-ticklemepink": makeCssHexColorDefinition("#fc89ac"),
