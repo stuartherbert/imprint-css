df2bbe7beec67960e95f74e93654b580856bd461
Stuart Herbert <stuart@stuartherbert.com>
2025-02-14 09:13:26 +0000

    IMPT-0013 New: added french-lilac color

--- packages/color-collections/src/colors/exports.ts
+++ packages/color-collections/src/colors/exports.ts
@@ -943,6 +943,7 @@ export const ENCYCOLORPEDIA_GARNET = ALL_COLORS["encycolorpedia-garnet"];
 export const ENCYCOLORPEDIA_GREENISHYELLOW = ALL_COLORS["encycolorpedia-greenishyellow"];
 export const ENCYCOLORPEDIA_TEAGREEN = ALL_COLORS["encycolorpedia-teagreen"];
 export const ENCYCOLORPEDIA_VERDIGRIS = ALL_COLORS["encycolorpedia-verdigris"];
+export const FRENCH_LILAC = ALL_COLORS["french-lilac"];
 export const FRENCH_PINK = ALL_COLORS["french-pink"];
 export const GO_GREEN = ALL_COLORS["go-green"];
 export const HUNGARIAN_GREEN = ALL_COLORS["hungarian-green"];
--- packages/color-collections/src/colors/misc.ts
+++ packages/color-collections/src/colors/misc.ts
@@ -148,6 +148,7 @@ export const MISC_COLORS: CssColorDefinitions = {
     "encycolorpedia-greenishyellow": makeCssHexColorDefinition("#eeea62"),
     "encycolorpedia-teagreen": makeCssHexColorDefinition("#d0f0c0"),
     "encycolorpedia-verdigris": makeCssHexColorDefinition("#43b3ae"),
+    "french-lilac": makeCssHexColorDefinition("#86608e"),
     "french-pink": makeCssHexColorDefinition("#fd6c9e"),
     "go-green": makeCssHexColorDefinition("#00ab66"),
     "hungarian-green": makeCssHexColorDefinition("#477050"),
