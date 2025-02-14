32732992082c82b32e9e442735802b1c8393af42
Stuart Herbert <stuart@stuartherbert.com>
2025-02-14 19:24:57 +0000

    IMPT-0013 New: added misc-palatinateblue color

--- packages/color-collections/src/colors/misc.ts
+++ packages/color-collections/src/colors/misc.ts
@@ -430,6 +430,7 @@ export const MISC_COLORS: CssColorDefinitions = {
     "misc-orangepeel": makeCssHexColorDefinition("#ff9f00"),
     "misc-oxblood": makeCssHexColorDefinition("#4a0000"),
     "misc-palatinate": makeCssHexColorDefinition("#72246C"),
+    "misc-palatinateblue": makeCssHexColorDefinition("#0038A8"),
     "misc-palatinatepurple": makeCssHexColorDefinition("#68246D"),
     "misc-paleyellow": makeCssHexColorDefinition("#fcf8e3"),
     "misc-parisgreen": makeCssHexColorDefinition("#50c878"),
