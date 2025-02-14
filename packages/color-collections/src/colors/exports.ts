6f1d913ebd71839bfb06e594175513500d6bd1c5
Stuart Herbert <stuart@stuartherbert.com>
2025-02-14 10:37:24 +0000

    IMPT-0013 New: added misc-darkbluegray color

--- packages/color-collections/src/colors/exports.ts
+++ packages/color-collections/src/colors/exports.ts
@@ -1081,6 +1081,7 @@ export const MISC_COOLBLACK = ALL_COLORS["misc-coolblack"];
 export const MISC_COQUELICOT = ALL_COLORS["misc-coquelicot"];
 export const MISC_CORALRED = ALL_COLORS["misc-coralred"];
 export const MISC_CORDOVAN = ALL_COLORS["misc-cordovan"];
+export const MISC_DARKBLUEGRAY = ALL_COLORS["misc-darkbluegray"];
 export const MISC_DARKESTMOSSGREEN = ALL_COLORS["misc-darkestmossgreen"];
 export const MISC_DARKMOSSGREEN = ALL_COLORS["misc-darkmossgreen"];
 export const MISC_DEEPSAFFRON = ALL_COLORS["misc-deepsaffron"];
--- packages/color-collections/src/colors/misc.ts
+++ packages/color-collections/src/colors/misc.ts
@@ -286,6 +286,7 @@ export const MISC_COLORS: CssColorDefinitions = {
     "misc-coquelicot": makeCssHexColorDefinition("#ff3800"),
     "misc-coralred": makeCssHexColorDefinition("#ff4040"),
     "misc-cordovan": makeCssHexColorDefinition("#893f45"),
+    "misc-darkbluegray": makeCssHexColorDefinition("#669"),
     "misc-darkestmossgreen": makeCssHexColorDefinition("#272809"),
     "misc-darkmossgreen": makeCssHexColorDefinition("#5d6f2d"),
     "misc-deepsaffron": makeCssHexColorDefinition("#ff9933"),
