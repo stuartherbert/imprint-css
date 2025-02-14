a0171cdbde55f6fe08bbda711e3733ee92034f82
Stuart Herbert <stuart@stuartherbert.com>
2025-02-14 11:16:56 +0000

    IMPT-0013 New: added iscc-nbs-mediumtuscanred color

--- packages/color-collections/src/colors/exports.ts
+++ packages/color-collections/src/colors/exports.ts
@@ -992,6 +992,7 @@ export const ISCC_NBS_LAVENDERGRAY = ALL_COLORS["iscc-nbs-lavendergray"];
 export const ISCC_NBS_LIBERTY = ALL_COLORS["iscc-nbs-liberty"];
 export const ISCC_NBS_LUST = ALL_COLORS["iscc-nbs-lust"];
 export const ISCC_NBS_MAUVETAUPE = ALL_COLORS["iscc-nbs-mauvetaupe"];
+export const ISCC_NBS_MEDIUMTUSCANRED = ALL_COLORS["iscc-nbs-mediumtuscanred"];
 export const ISCC_NBS_MORNINGBLUE = ALL_COLORS["iscc-nbs-morningblue"];
 export const ISCC_NBS_MOSSGREEN = ALL_COLORS["iscc-nbs-mossgreen"];
 export const ISCC_NBS_MYRTLEGREEN = ALL_COLORS["iscc-nbs-myrtlegreen"];
--- packages/color-collections/src/colors/misc.ts
+++ packages/color-collections/src/colors/misc.ts
@@ -197,6 +197,7 @@ export const MISC_COLORS: CssColorDefinitions = {
     "iscc-nbs-liberty": makeCssHexColorDefinition("#545aa7"),
     "iscc-nbs-lust": makeCssHexColorDefinition("#e62020"),
     "iscc-nbs-mauvetaupe": makeCssHexColorDefinition("#915f6d"),
+    "iscc-nbs-mediumtuscanred": makeCssHexColorDefinition("#79443b"),
     "iscc-nbs-morningblue": makeCssHexColorDefinition("#8da399"),
     "iscc-nbs-mossgreen": makeCssHexColorDefinition("#8a9a5b"),
     "iscc-nbs-myrtlegreen": makeCssHexColorDefinition("#317873"),
