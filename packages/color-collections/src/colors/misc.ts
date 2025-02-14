10b84b35053f36b295357af70bdeed66c49b3ca5
Stuart Herbert <stuart@stuartherbert.com>
2025-02-14 12:22:26 +0000

    IMPT-0013 New: added resene-vanillaice color

--- packages/color-collections/src/colors/misc.ts
+++ packages/color-collections/src/colors/misc.ts
@@ -490,6 +490,7 @@ export const MISC_COLORS: CssColorDefinitions = {
     "resene-rocketmetallic": makeCssHexColorDefinition("#8a7f8d"),
     "resene-romansilver": makeCssHexColorDefinition("#838996"),
     "resene-spacecadet": makeCssHexColorDefinition("#1e2952"),
+    "resene-vanillaice": makeCssHexColorDefinition("#f3d9df"),
     "resene-xanadu": makeCssHexColorDefinition("#738678"),
     "resene-zomp": makeCssHexColorDefinition("#39a78d"),
     "ridgeway-woodbrown": makeCssHexColorDefinition("#c19a6b"),
