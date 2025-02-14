dab3485f859c7fea9498cf34dfae64997abd4c8c
Stuart Herbert <stuart@stuartherbert.com>
2025-02-14 12:00:39 +0000

    IMPT-0013 New: added pourpre-bistre color

--- packages/color-collections/src/colors/exports.ts
+++ packages/color-collections/src/colors/exports.ts
@@ -1258,6 +1258,7 @@ export const PLOCHERE_QUEENPINK = ALL_COLORS["plochere-queenpink"];
 export const PLOCHERE_SILVERPINK = ALL_COLORS["plochere-silverpink"];
 export const PLOCHERE_TANGOPINK = ALL_COLORS["plochere-tangopink"];
 export const POURPRE_AVOCADO = ALL_COLORS["pourpre-avocado"];
+export const POURPRE_BISTRE = ALL_COLORS["pourpre-bistre"];
 export const POURPRE_CARMINE = ALL_COLORS["pourpre-carmine"];
 export const POURPRE_CHALKWHITE = ALL_COLORS["pourpre-chalkwhite"];
 export const POURPRE_ELECTRICBLUE = ALL_COLORS["pourpre-electricblue"];
--- packages/color-collections/src/colors/misc.ts
+++ packages/color-collections/src/colors/misc.ts
@@ -463,6 +463,7 @@ export const MISC_COLORS: CssColorDefinitions = {
     "plochere-silverpink": makeCssHexColorDefinition("#c4aead"),
     "plochere-tangopink": makeCssHexColorDefinition("#e4717a"),
     "pourpre-avocado": makeCssHexColorDefinition("#568203"),
+    "pourpre-bistre": makeCssHexColorDefinition("#856d4d"),
     "pourpre-carmine": makeCssHexColorDefinition("#960018"),
     "pourpre-chalkwhite": makeCssHexColorDefinition("#fbffff"),
     "pourpre-electricblue": makeCssHexColorDefinition("#2c75ff"),
