09fc2aafaee5119b1751d307f4da73e9d6ec55ca
Stuart Herbert <stuart@stuartherbert.com>
2025-02-14 17:56:36 +0000

    IMPT-0013 New: added french-rose color

--- packages/color-collections/src/colors/exports.ts
+++ packages/color-collections/src/colors/exports.ts
@@ -961,6 +961,7 @@ export const FRENCH_ECRU = ALL_COLORS["french-ecru"];
 export const FRENCH_LIGHTBEIGE = ALL_COLORS["french-lightbeige"];
 export const FRENCH_LILAC = ALL_COLORS["french-lilac"];
 export const FRENCH_PINK = ALL_COLORS["french-pink"];
+export const FRENCH_ROSE = ALL_COLORS["french-rose"];
 export const GO_GREEN = ALL_COLORS["go-green"];
 export const HUNGARIAN_GREEN = ALL_COLORS["hungarian-green"];
 export const HUNYADI_YELLOW = ALL_COLORS["hunyadi-yellow"];
