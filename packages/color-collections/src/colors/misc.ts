b25111f5454c8464f02f1b619683e421fb46e254
Stuart Herbert <stuart@stuartherbert.com>
2025-02-15 13:21:20 +0000

    IMPT-0013 New: added misc-goldenpoppy color

--- packages/color-collections/src/colors/misc.ts
+++ packages/color-collections/src/colors/misc.ts
@@ -411,6 +411,7 @@ export const MISC_COLORS: CssColorDefinitions = {
     "misc-folly": makeCssHexColorDefinition("#ff004f"),
     "misc-fuchsiapurple": makeCssHexColorDefinition("#cc397b"),
     "misc-giantsorange": makeCssHexColorDefinition("#fe5a1d"),
+    "misc-goldenpoppy": makeCssHexColorDefinition("#fcc200"),
     "misc-goldenyellow": makeCssHexColorDefinition("#ffdf00"),
     "misc-graycard": makeCssHexColorDefinition("#7c7c7c"),
     "misc-greenearth": makeCssHexColorDefinition("#dadd98"),
