8d065875b25b64c1ce30837c088467ce47fa278b
Stuart Herbert <stuart@stuartherbert.com>
2025-02-15 10:21:54 +0000

    IMPT-0013 New: added plochere-beaublue color

--- packages/color-collections/src/colors/exports.ts
+++ packages/color-collections/src/colors/exports.ts
@@ -1351,6 +1351,7 @@ export const PERSIAN_PLUM = ALL_COLORS["persian-plum"];
 export const PERSIAN_RED = ALL_COLORS["persian-red"];
 export const PERSIAN_ROSE = ALL_COLORS["persian-rose"];
 export const PLOCHERE_ANTIQUEFUCHSIA = ALL_COLORS["plochere-antiquefuchsia"];
+export const PLOCHERE_BEAUBLUE = ALL_COLORS["plochere-beaublue"];
 export const PLOCHERE_CHARMPINK = ALL_COLORS["plochere-charmpink"];
 export const PLOCHERE_CHINAPINK = ALL_COLORS["plochere-chinapink"];
 export const PLOCHERE_IRRESISTIBLE = ALL_COLORS["plochere-irresistible"];
