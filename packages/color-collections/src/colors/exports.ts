c83314754cf7589e5352baaccf8372749e271cb6
Stuart Herbert <stuart@stuartherbert.com>
2025-02-15 11:18:18 +0000

    IMPT-0013 New: added misc-tangblue color

--- packages/color-collections/src/colors/exports.ts
+++ packages/color-collections/src/colors/exports.ts
@@ -1321,6 +1321,7 @@ export const MISC_SHOCKINGPINK = ALL_COLORS["misc-shockingpink"];
 export const MISC_SKYMAGENTA = ALL_COLORS["misc-skymagenta"];
 export const MISC_SOLIDWHITE = ALL_COLORS["misc-solidwhite"];
 export const MISC_SPLASHEDWHITE = ALL_COLORS["misc-splashedwhite"];
+export const MISC_TANGBLUE = ALL_COLORS["misc-tangblue"];
 export const MISC_TANGELO = ALL_COLORS["misc-tangelo"];
 export const MISC_TANGERINE = ALL_COLORS["misc-tangerine"];
 export const MISC_TEALBLUE = ALL_COLORS["misc-tealblue"];
