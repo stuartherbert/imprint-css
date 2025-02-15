1ccded0fda6341bcd1dd482ff440df5583203067
Stuart Herbert <stuart@stuartherbert.com>
2025-02-15 14:04:07 +0000

    IMPT-0013 New: added iscc-nbs-darkchampagne color

--- packages/color-collections/src/colors/exports.ts
+++ packages/color-collections/src/colors/exports.ts
@@ -1034,6 +1034,7 @@ export const ISCC_NBS_CHINESERED = ALL_COLORS["iscc-nbs-chinesered"];
 export const ISCC_NBS_COOLGRAY = ALL_COLORS["iscc-nbs-coolgray"];
 export const ISCC_NBS_CORALPINK = ALL_COLORS["iscc-nbs-coralpink"];
 export const ISCC_NBS_DARKBYZANTIUM = ALL_COLORS["iscc-nbs-darkbyzantium"];
+export const ISCC_NBS_DARKCHAMPAGNE = ALL_COLORS["iscc-nbs-darkchampagne"];
 export const ISCC_NBS_DARKELECTRICBLUE = ALL_COLORS["iscc-nbs-darkelectricblue"];
 export const ISCC_NBS_DARKPURPLE = ALL_COLORS["iscc-nbs-darkpurple"];
 export const ISCC_NBS_DAVYSGREY = ALL_COLORS["iscc-nbs-davysgrey"];
--- packages/color-collections/src/colors/misc.ts
+++ packages/color-collections/src/colors/misc.ts
@@ -239,6 +239,7 @@ export const MISC_COLORS: CssColorDefinitions = {
     "iscc-nbs-coolgray": makeCssHexColorDefinition("#9090c0"),
     "iscc-nbs-coralpink": makeCssHexColorDefinition("#f88379"),
     "iscc-nbs-darkbyzantium": makeCssHexColorDefinition("#5d3954"),
+    "iscc-nbs-darkchampagne": makeCssHexColorDefinition("#c2b280"),
     "iscc-nbs-darkelectricblue": makeCssHexColorDefinition("#536878"),
     "iscc-nbs-darkpurple": makeCssHexColorDefinition("#301934"),
     "iscc-nbs-davysgrey": makeCssHexColorDefinition("#555"),
