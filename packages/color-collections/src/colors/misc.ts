4d7a9b030544f49e5f06274162cebfdc1c34d3b1
Stuart Herbert <stuart@stuartherbert.com>
2025-02-15 13:53:14 +0000

    IMPT-0013 New: added misc-liontawny1, misc-liontawny2 colors

--- packages/color-collections/src/colors/misc.ts
+++ packages/color-collections/src/colors/misc.ts
@@ -454,6 +454,8 @@ export const MISC_COLORS: CssColorDefinitions = {
     "misc-lightchocolate": makeCssHexColorDefinition("#cc7f33"),
     "misc-lightmossgreen": makeCssHexColorDefinition("#b9bd78"),
     "misc-lilac": makeCssHexColorDefinition("#c8a2c8"),
+    "misc-liontawny1": makeCssHexColorDefinition("#c19a6b"),
+    "misc-liontawny2": makeCssHexColorDefinition("#826644"),
     "misc-liseranpurple": makeCssHexColorDefinition("#de6fa1"),
     "misc-lustygallant": makeCssHexColorDefinition("#ffcccc"),
     "misc-madder": makeCssHexColorDefinition("#a50021"),
