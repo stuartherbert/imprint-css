2bf10b5dd1d5a9425a13ad35074868981f74303b
Stuart Herbert <stuart@stuartherbert.com>
2025-02-15 15:52:54 +0000

    IMPT-0013 New: added iscc-nbs-puce color

--- packages/color-collections/src/colors/exports.ts
+++ packages/color-collections/src/colors/exports.ts
@@ -1109,6 +1109,7 @@ export const ISCC_NBS_PASTELPINK = ALL_COLORS["iscc-nbs-pastelpink"];
 export const ISCC_NBS_PEACOCKBLUE = ALL_COLORS["iscc-nbs-peacockblue"];
 export const ISCC_NBS_PISTACHIO = ALL_COLORS["iscc-nbs-pistachio"];
 export const ISCC_NBS_POMPANDPOWER = ALL_COLORS["iscc-nbs-pompandpower"];
+export const ISCC_NBS_PUCE = ALL_COLORS["iscc-nbs-puce"];
 export const ISCC_NBS_PURPUREUS = ALL_COLORS["iscc-nbs-purpureus"];
 export const ISCC_NBS_QUEENBLUE = ALL_COLORS["iscc-nbs-queenblue"];
 export const ISCC_NBS_RAISINBLACK = ALL_COLORS["iscc-nbs-raisinblack"];
--- packages/color-collections/src/colors/misc.ts
+++ packages/color-collections/src/colors/misc.ts
@@ -314,6 +314,7 @@ export const MISC_COLORS: CssColorDefinitions = {
     "iscc-nbs-peacockblue": makeCssHexColorDefinition("#004958"),
     "iscc-nbs-pistachio": makeCssHexColorDefinition("#93c572"),
     "iscc-nbs-pompandpower": makeCssHexColorDefinition("#86608e"),
+    "iscc-nbs-puce": makeCssHexColorDefinition("#722f37"),
     "iscc-nbs-purpureus": makeCssHexColorDefinition("#9a4eae"),
     "iscc-nbs-queenblue": makeCssHexColorDefinition("#436b95"),
     "iscc-nbs-raisinblack": makeCssHexColorDefinition("#242124"),
