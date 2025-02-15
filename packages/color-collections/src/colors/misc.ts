97ff6c601d9361f66ca40c954f12062b34a8a6cc
Stuart Herbert <stuart@stuartherbert.com>
2025-02-15 17:37:42 +0000

    IMPT-0013 New: added misc-cadmiumyellow color

--- packages/color-collections/src/colors/misc.ts
+++ packages/color-collections/src/colors/misc.ts
@@ -424,6 +424,7 @@ export const MISC_COLORS: CssColorDefinitions = {
     "misc-cadet": makeCssHexColorDefinition("#536872"),
     "misc-cadmiumorange": makeCssHexColorDefinition("#ed872d"),
     "misc-cadmiumred": makeCssHexColorDefinition("#e30022"),
+    "misc-cadmiumyellow": makeCssHexColorDefinition("#fff600"),
     "misc-canaryyellow": makeCssHexColorDefinition("#ff9"),
     "misc-candlelight": makeCssHexColorDefinition("#f8d810"),
     "misc-candyapplered": makeCssHexColorDefinition("#ff0800"),
