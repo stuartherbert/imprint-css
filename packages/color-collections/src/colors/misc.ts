cf8779d46acefba76c54f9b8602a98be96c5e5b9
Stuart Herbert <stuart@stuartherbert.com>
2025-02-15 14:16:27 +0000

    IMPT-0013 Fix: rename crayola-lemon to crayola-lemonglacier

--- packages/color-collections/src/colors/misc.ts
+++ packages/color-collections/src/colors/misc.ts
@@ -115,7 +115,7 @@ export const MISC_COLORS: CssColorDefinitions = {
     "crayola-jasper": makeCssHexColorDefinition("#d05340"),
     "crayola-junglegreen": makeCssHexColorDefinition("#29ab87"),
     "crayola-lavender": makeCssHexColorDefinition("#fbaed2"),
-    "crayola-lemon": makeCssHexColorDefinition("#fdff00"),
+    "crayola-lemonglacier": makeCssHexColorDefinition("#fdff00"),
     "crayola-licorice": makeCssHexColorDefinition("#1a1110"),
     "crayola-lightblue": makeCssHexColorDefinition("#8fd8d8"),
     "crayola-lightorange": makeCssHexColorDefinition("#fed8b1"),
