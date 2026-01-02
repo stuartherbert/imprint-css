33c91406e97d13afb6a6f3dcd846a785fa90ca8c
Stuart Herbert <stuart@stuartherbert.com>
2026-01-02 21:07:15 +0000

    Fix: add config for sizingUnitSizes (b/c break)

--- packages/docs/src/content/config.ts
+++ packages/docs/src/content/config.ts
@@ -1,12 +1,12 @@
 // Import utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
-import { docsSchema } from "../schemas/doc";
 import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+import { colorsSchema } from "../schemas/color";
+import { colorAliasSchema } from "../schemas/colorAlias";
 import { colorGroupSchema } from "../schemas/colorGroup";
 import { colorHueSchema } from "../schemas/colorHue";
 import { colorPaletteSchema } from "../schemas/colorPalette";
-import { colorsSchema } from "../schemas/color";
-import { colorAliasSchema } from "../schemas/colorAlias";
+import { docsSchema } from "../schemas/doc";
 
 const CssStyles = defineCollection({
     type: 'data'
@@ -68,6 +68,11 @@ const colorPalettes = defineCollection({
     schema: colorPaletteSchema,
 });
 
+const sizingUnitSets = defineCollection({
+    type: "data",
+    schema: z.object({}).catchall(z.string()),
+});
+
 export const collections = {
     colors,
     colorAliases,
@@ -77,5 +82,6 @@ export const collections = {
     devices,
     docs,
     CssStyles,
-    CssVars
+    CssVars,
+    sizingUnitSets,
 };
